615e00730d801d5eeacedbc5596f5965f628f1a6
floreacalin <floreacalin1@gmail.com>
2022-06-24 08:20:47 +0300

    commit de la lucru sper sa mearga.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import AddUser from "./components/Users/AddUser";
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import Form from "./Components/Homework/form";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Homework/form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <div>
+      <li>
+        <p> Mama Mia </p>
+        <p> Mama Miu </p>
+      </li>
+    </div>
+  );
+};
+
+export default Form;
